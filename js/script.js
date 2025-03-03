11022671409814ef9bb0a1bd22df0378f952d333
Patel Mark <patelmark5330@gmail.com>
2025-03-03 10:00:40 +0530

    main

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 25000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    loop: true, /* Ensure continuous looping */
+  });
+  
